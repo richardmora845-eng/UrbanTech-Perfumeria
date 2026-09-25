69b9f01ac2a20c843695cbedb65d913ab471e171
richardmora845-eng <richardmora845@gmail.com>
2026-09-25 11:38:38 -0600

    Add Supabase URL and keys to config.js

--- config.js
+++ config.js
@@ -1,1 +1,3 @@
-https://khvrpnfgkikgysludpod.supabase.co
+const SUPABASE_URL = "https://khvrpnfgkikgysludpod.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_nHvdaVoYINUMmTUwmST2LQ_q4O02VnY";
+const WHATSAPP = "50670461262";
